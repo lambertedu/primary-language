a6ba8a71bf5e7911153194ecce6b7f26500f288f
Lambert Kakraba <lambertedudzi95@gmail.com>
2023-10-09 23:55:27 +0000

    first commit on variables

---
+++ variables.py
@@ -0,0 +1,10 @@
+character_name = "George"
+character_age = "70"
+print("There once a man name " +character_name +", ")
+print("he was " +character_age+" years old. ")
+
+character_name = "Elorm"
+print("He really liked the "+character_name+", ")
+
+character_age = "50"
+print("but din't like being " +character_age)
