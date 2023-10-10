ad9fa51ae3c74feb632bed1c71a4f55071d0ecac
Lambert Kakraba <lambertedudzi95@gmail.com>
2023-10-10 03:25:18 +0000

    just added few math function to my code

---
+++ number.py
@@ -0,0 +1,16 @@
+from math import *
+my_num = -5
+#covert the number to string
+print(str(my_num))
+
+#print the absolute number
+print(abs(my_num))
+
+#exponential numbers
+print(pow(1, 4 ))
+#returns the larger of the two numbers
+print(max(3, 4))
+print(min(10, 56))
+print(round(3.5))
+print(ceil(4.4))
+print(sqrt(40))
