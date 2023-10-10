ddd350f7c13ebcf75115c0e0b1844f8d36459bfc
Lambert Kakraba <lambertedudzi95@gmail.com>
2023-10-10 02:11:55 +0000

    I've added new string function

--- strings.py
+++ strings.py
@@ -1,4 +1,8 @@
 phrase = "Lambert Academy"
 print(phrase.lower())
 print(phrase.upper())
-print(phrase.isupper)
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("A"))
