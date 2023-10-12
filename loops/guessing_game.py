5fe66f04bafc8985755d49c609b3838352bcb8bd
Lambert Kakraba <lambertedudzi95@gmail.com>
2023-10-12 04:04:17 +0000

    made the game a little more intelligent

--- loops/guessing_game.py
+++ loops/guessing_game.py
@@ -1,7 +1,16 @@
 secrete_word = "rofina"
 guess = ""
+guess_count = 0
+out_of_guesses = False
+guess_limit = 3
 
-while guess != secrete_word:
-    guess = input("Enter guess: ") 
-
-print("You got it")
+while guess != secrete_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter guess: ") 
+        guess_count +=1
+    else:
+        out_of_guesses = True
+if out_of_guesses:
+    print("You lose")    
+else :    
+    print("You got it")
