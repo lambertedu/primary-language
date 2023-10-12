3604bf70470506fe3b3a5eeece9e50564d203e88
Lambert Kakraba <lambertedudzi95@gmail.com>
2023-10-12 03:25:54 +0000

    a loop and dictionary

---
+++ function_loop/dictionary.py
@@ -0,0 +1,18 @@
+months = {
+    "Jan" :"January",
+    "Feb" : "February",
+    "Mar" : "March",
+    "Apr" : "Apriel",
+    "May" : "May",
+    "Jun" : "June",
+    "Jul" : "July",
+    "Aug" : "August",
+    "Sep" : "September",
+    "oct" : "october",
+    "Nov" : "November",
+    "Dec" : "December",
+     
+}
+
+print(months["Nov"])
+print(months.get("Dec", "Not a valid key"))
