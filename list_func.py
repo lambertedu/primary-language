853b69875f6900d6c590edf12d81ad36d5e83e45
Lambert Kakraba <lambertedudzi95@gmail.com>
2023-10-12 01:55:48 +0000

    added tuple, list, and if statement

--- list_func.py
+++ list_func.py
@@ -1,13 +1,14 @@
 lucky_number = [4, 8, 15, 16, 23, 42]
-friend = ["Kevin", "Karen", "jim", "Oscar","Toby"]
-friend.extend(lucky_number)
+friend = ["Kevin", "Karen", "jim", "Oscar","Toby", "Elorm"]
+
 friend.append("Creap")
 friend.insert(2, "Mark")
 friend.remove("Elorm")
-friend.clear()
 friend.pop()
 friend.count("Jim")
-friend.sort()
+lucky_number.sort()
 lucky_number.reverse()
 friend2 = friend.copy()
+friend.extend(lucky_number)
+friend.clear()
 print(friend)
