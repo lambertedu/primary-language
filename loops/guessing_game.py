a6b6f416c563881568a5fe1a142c6fa2a79e835d
Lambert Kakraba <lambertedudzi95@gmail.com>
2023-10-13 13:48:23 +0000

    changed it to a method

--- loops/guessing_game.py
+++ loops/guessing_game.py
@@ -1,16 +1,20 @@
-secrete_word = "rofina"
-guess = ""
-guess_count = 0
-out_of_guesses = False
-guess_limit = 3
+def guessing_game():
+    secret_word = "rofina"
+    guess = ""
+    guess_limit = 4
+    guess_count = 0
+    out_of_guesses = False
 
-while guess != secrete_word and not(out_of_guesses):
-    if guess_count < guess_limit:
-        guess = input("Enter guess: ") 
-        guess_count +=1
+    while  guess != secret_word and not(out_of_guesses):
+        if guess_count < guess_limit:
+            guess = input("Enter guess: ") 
+            guess_count += 1
+        else:
+            out_of_guesses = True
+            
+    if  out_of_guesses:
+        print("Out of guesses, You lose!")
     else:
-        out_of_guesses = True
-if out_of_guesses:
-    print("You lose")    
-else :    
-    print("You got it")
+        print("You got it")
+
+guessing_game()
