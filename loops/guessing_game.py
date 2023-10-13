18da9b68bb9420f3cdbc4463d250bc401eb24a78
Lambert Kakraba <lambertedudzi95@gmail.com>
2023-10-13 14:04:53 +0000

    added an empty space

--- loops/guessing_game.py
+++ loops/guessing_game.py
@@ -5,16 +5,17 @@ def guessing_game():
     guess_count = 0
     out_of_guesses = False
 
-    while  guess != secret_word and not(out_of_guesses):
+    while guess != secret_word and not (out_of_guesses):
         if guess_count < guess_limit:
-            guess = input("Enter guess: ") 
+            guess = input("Enter guess: ")
             guess_count += 1
         else:
             out_of_guesses = True
-            
-    if  out_of_guesses:
+
+    if out_of_guesses:
         print("Out of guesses, You lose!")
     else:
         print("You got it")
+
 
 guessing_game()
