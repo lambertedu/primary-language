73938e8105cebfb2a91fba28e734de327b801814
Lambert Kakraba <lambertedudzi95@gmail.com>
2023-10-16 23:48:40 +0000

    added for loop and exponential function

---
+++ loops/for_loop.py
@@ -0,0 +1,30 @@
+b = "Lambert"
+c = "Rofina"
+
+
+for letter in "lambert":
+    print(letter)
+
+
+friends = ["Lambert", "Edudzi", "Elorm", 7, 8]
+for friend in friends:
+    print(friend)
+
+
+for index in range(100):
+    print(index)
+
+
+for index in range(4, 17):
+    print(index)
+
+
+for index in range(len(friends)):
+    print(friends[index])
+
+
+for index in range(7):
+    if index == 0:
+        print(b + " and " + c + " are in.....well, you know the rest")
+    else:
+        print(index)
